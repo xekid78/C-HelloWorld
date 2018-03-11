10da6651b2920e3033b2e2eb3d5ea5c56734e838
Makoto Tanabe <xekid78@outlook.com>
2018-03-11 20:01:38 +0900

    Add files via upload

---
+++ Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
