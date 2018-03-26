a5805dc86a364b9d8a96521b939e6a536c63a4b4
Makoto Tanabe <xekid78@outlook.com>
2018-03-26 09:42:00 +0900

    Add files via upload

---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Hello
+{
+    class Hello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // 実行結果確認のための処理 ここから
+            Console.WriteLine();
+            Console.WriteLine("何かキーを押してください");
+            Console.ReadKey();
+            // ここまで
+        }
+    }
+}
