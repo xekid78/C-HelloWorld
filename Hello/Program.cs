be28058a6ff7aefeb56acaaf050409c39094be74
Makoto Tanabe <xekid78@outlook.com>
2018-03-27 14:38:46 +0900

    Add files via upload

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -6,13 +6,7 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            // 実行結果確認のための処理 ここから
-            Console.WriteLine();
-            Console.WriteLine("何かキーを押してください");
-            Console.ReadKey();
-            // ここまで
+            Console.WriteLine("Hello World");
         }
     }
 }
